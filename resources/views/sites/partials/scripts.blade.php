8cf4d384307f2197141a165a5895aa4aa3d3aea0
thanhhungmk92 <thanhhungmk92@gmail.com>
2018-12-06 15:16:45 +0700

    landing-page theme

---
+++ resources/views/sites/partials/scripts.blade.php
@@ -0,0 +1,20 @@
+<?php
+    echo scripts([
+        'js/libraries/jquery-1.12.4.min.js',
+        'js/libraries/bootstrap.min.js',
+        'bower_components/wow/dist/wow.js',
+        'bower_components/swiper/dist/js/swiper.min.js',
+        'bower_components/magnific-popup/dist/jquery.magnific-popup.min.js'
+    ]);
+
+    // MY SCRIPTS
+    echo scripts([
+        'js/libraries/swiper.min.js',
+        'js/libraries/jquery.slicknav.js',
+        'js/libraries/responsive-tabs.js',
+        'js/libraries/validator.min.js',
+        'js/libraries/SmoothScroll.js',
+        'js/libraries/function.js'
+
+    ]);
+?>

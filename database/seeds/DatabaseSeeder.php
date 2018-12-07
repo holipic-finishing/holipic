c388635954033e201b3652e5fe764d1ee7682d9e
thanhhungmk92 <thanhhungmk92@gmail.com>
2018-12-07 18:35:03 +0700

    seeder user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,20 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
+
         // $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-=======
+
 		DB::table('users')->insert([
             'company_name' =>'holipic',
             'first_name' => 'hoa_map',
             'last_name' => 'tran',
             'email' =>'tranduyhoa94@gmail.com',
-            'password' => Hash::make('123465'),
+            'password' => Hash::make('12345'),
             'access_token' => 'Wado4p88M8GwUk7aSi4v7LnAuokXbgnNJJ2fipzPzL7U4r2VjtuBItjherSErMbR',
             'package_id' => 1,
             'role_id' => 1,            
         ]);
->>>>>>> 9823f81593ee09ebc45d2075a9a0b37b91013d30
+
     }
 }

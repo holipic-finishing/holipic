556a3b9c79c78a9205921f33592db93db24a99a5
Danh Le Thanh <lethanhdanh.it@gmail.com>
2019-01-05 11:37:15 +0700

    fix css change password

--- resources/js/views/users/ChangePassword.vue
+++ resources/js/views/users/ChangePassword.vue
@@ -1,66 +1,48 @@
 <template>
-    <v-container fluid pt-0 grid-list-xl>
-        <div class="panel-changepass">
-             <v-layout row wrap >
-                  <app-card
-                      colClasses="xl12 lg12 md12 sm12 xs12"
-                      customClasses="mb-0 sales-widget app-changepass"
-                      :fullScreen="true"
-                      :reloadable="true"
-                      :closeable="false"
-                    >
-                    <!-- <div class="container fix-container"> -->
-                         <!-- <page-title-bar></page-title-bar> -->
-                        <!--  <div class="content-row"> -->
-                               <!--  <app-card
-                                  :heading="$t('message.changePassword')"
-                                  :fullScreen="true"  
-                                  colClasses="xl12 lg12 md12 sm12 xs12"
-                                    > -->
-                                   <!--  <v-divider></v-divider> -->
-                                   <div class="row-changepass elevation-5">
-                                       <v-toolbar flat color="white">
-                                        <v-toolbar-title>
-                                          <span>Change Password</span>
-                                          
-                                        </v-toolbar-title>
-                                        </v-toolbar>
-                                      <v-divider></v-divider>
-                                        <v-container fluid grid-list-xl pt-0>
-                                            <v-form ref="form" @submit.prevent="savePassword">
-                                                <v-text-field 
-                                                  label="Current password" 
-                                                  v-model="oldPassword" 
-                                                  type="password" 
-                                                  :rules="[rules.required]"
-                                                ></v-text-field>
-                                                <v-text-field 
-                                                  label="New password" 
-                                                  v-model="newPassword" 
-                                                  type="password" 
-                                                 :rules="[rules.required]"
-                                                ></v-text-field>
-                                                <v-text-field 
-                                                  label="Confirm password" 
-                                                  v-model="confirmPassword" 
-                                                  type="password" 
-                                                  :rules="[rules.required]"
-                                                ></v-text-field>
-                                                <v-btn large type="submit" block color="primary">Submit Change</v-btn>
-                                            </v-form>
-                                        </v-container> 
-
-                                   </div>
-                                   
-                                <!-- </app-card>  -->
-                         <!-- </div> -->
-                       
-                    <!-- </div> -->
-                    </app-card>
-        </v-layout>
-
-        </div>
-       
+    <v-container fluid pt-0 class="h-100">
+        <div class="panel-changepass h-100 elevation-5">
+            <v-layout row wrap fill-height>
+                <app-card
+                    colClasses="xl12 lg12 md12 sm12 xs12"
+                    customClasses="mb-0 sales-widget app-changepass"
+                    :fullScreen="true"
+                    :reloadable="true"
+                    :closeable="false"
+                >
+                    <div class="row-changepass elevation-5">
+                        <v-toolbar flat color="white">
+                            <v-toolbar-title>
+                                <span>Change Password</span>
+                            </v-toolbar-title>
+                        </v-toolbar>
+                        <v-divider></v-divider>
+                        <v-container fluid grid-list-xl pt-0>
+                              <v-form ref="form" @submit.prevent="savePassword">
+                                  <v-text-field 
+                                    label="Current password" 
+                                    v-model="oldPassword" 
+                                    type="password" 
+                                    :rules="[rules.required]"
+                                  ></v-text-field>
+                                  <v-text-field 
+                                    label="New password" 
+                                    v-model="newPassword" 
+                                    type="password" 
+                                   :rules="[rules.required]"
+                                  ></v-text-field>
+                                  <v-text-field 
+                                    label="Confirm password" 
+                                    v-model="confirmPassword" 
+                                    type="password" 
+                                    :rules="[rules.required]"
+                                  ></v-text-field>
+                                  <v-btn large type="submit" block color="primary">Submit Change</v-btn>
+                              </v-form>
+                        </v-container> 
+                     </div>
+                </app-card>
+            </v-layout>
+        </div>       
     </v-container>
 
 </template>
@@ -113,22 +95,26 @@ export default {
         width: 100%;
         height: 100%;
         text-align: center;
-
     }
+    
     .content-row {
         display:flex;
         position: relative;
         top: 25%;
         padding: 0px 150px;
     }
-   @media (max-width: 760px) {
+    
+    @media (max-width: 760px) {
         .content-row {
             padding:0px 30px;
         }
     }
 
     .panel-changepass{
-        margin-top:30px;
-       
-    }    
+        margin: 30px 15px;
+    } 
+
+    .h-90{
+        height: 90% !important;
+    }   
 </style>

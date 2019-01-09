b020750271072e4a5455baf9deeb857b18858fa4
Ocean <Email ca bnnguyenhuudaiduong14t3@gmail.com>
2019-01-09 11:45:21 +0700

    fix_bugs

---
+++ app/Repositories/CustomerRepository.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Customer;
+use InfyOm\Generator\Common\BaseRepository;
+
+/**
+ * Class CustomerRepository
+ * @package App\Repositories
+ * @version January 5, 2019, 9:11 am UTC
+ *
+ * @method Customer findWithoutFail($id, $columns = ['*'])
+ * @method Customer find($id, $columns = ['*'])
+ * @method Customer first($columns = ['*'])
+*/
+class CustomerRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name',
+        'room',
+        'email',
+        'password',
+        'avatar'
+    ];
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Customer::class;
+    }
+
+    public function handelGetCustomers()
+    {
+        $company = \App\Models\Company::whereOwnerId(request('userId'))->first();
+        $data = [];
+        
+        if(!empty($company)) {
+            $customers = $this->model->with('user')->with('room')->with(['branch' => function($q) use ($company) {
+                                     $q->whereCompanyId($company['id']);
+            }])->get()->toArray();
+
+            foreach($customers as $customer) 
+            {
+                if($customer['branch'] != null)
+                {
+                    $data[] = $customer;
+                }
+            }
+
+            return $data;
+        }
+
+        return false;
+    }
+
+    public function handleUpdateCustomer($id)
+    {
+        $customer = $this->model->findOrFail($id);
+
+        $input = request('params');
+
+        if(request('params.status')) {
+            $input = request('params.status') == 'Active' ? ['status' => true] : ['status' => false] ;
+        }
+
+        if(request()->file('avatar')) {
+
+            $image = request()->file('avatar');
+
+            $typeFile = $image->getClientOriginalExtension();
+
+            if($typeFile != 'jpg' && $typeFile != 'png' && $typeFile != 'jpeg'){
+                return false;
+            }
+
+            $name = time().'_'.$customer['id'].'_'.$image->getClientOriginalName();
+            $input = ['avatar' => '/images/customer/'.$name];
+
+            $destinationPath = public_path('/images/customer');
+            $image->move($destinationPath, $name);
+        }
+
+        if(!is_null($customer))
+        {
+            $customer = $customer->update($input);
+
+            return $customer;
+        }
+
+        return false;
+    }
+
+    public function handelGetCustomerInBranch()
+    {
+        $company = \App\Models\Company::whereOwnerId(request('userId'))->first();
+        $branch = \App\Models\Branch::where('company_id', '=', $company['id'])->where('user_id', '=', request('userId'))->get()->toArray();
+        dd($branch);
+        $data = [];
+        
+        if(!empty($company)) {
+            $customers = $this->model->with('user')->with('branch')->where('user_id', '=', request('userId'))->get()->toArray();
+dd($customers);
+            foreach($customers as $customer) 
+            {
+                if($customer['branch'] != null)
+                {
+                    $data[] = $customer;
+                }
+            }
+
+            return $data;
+        }
+
+        return false;
+    }
+}

4db6b5f0312baca63f5975891dcca44b99bfd2f2
thong <baibuvietnam@gmail.com>
2019-09-30 17:19:23 +0700

    fixed ui of customer login and purchase page

--- resources/js/views/customer/Login.vue
+++ resources/js/views/customer/Login.vue
@@ -1,15 +1,17 @@
 <template>
 	<v-container class="v-container-customer-login">
 		<div class="box-shawdom-login">
-			<div class="custom-box-shawdom">
+			<div class="custom-box-shawdom row">
 				<div class="col-md-8 left-col-login">
 				<v-img src="customer/loginimage.jpg" max-width="100%" max-height="100%"/>
 				</div>
 				<div class="col-md-4 right-col-login">
 					<v-card  tile flat >
 			          	<v-card-title primary-title class="v-card-title-h1">WELCOME</v-card-title>
-			          		<v-img src="shop_selling/logo-holipic-new.png" width="150px" style="display: inline-block;"/>
-			          	<v-card-text v-if="checkFormLogin">Login to Start Selling Photos</v-card-text>
+			          		<v-img src="shop_selling/logo-holipic-new.png" width="100px" style="display: inline-block;"/>
+			          	<!-- <v-card-text v-if="checkFormLogin">Login to Start Selling Photos</v-card-text> -->
+			          	<v-card-text v-if="checkFormLogin">Login to Download and Purchase Photos</v-card-text>
+
 			          	<v-card-text v-if="checkFormPassword">Reset Password to Start Selling Photos</v-card-text>
 			          	<v-card tile flat  class="v-card-shop">
 			          		<div class="custom-shop-form" style="color:black !important;">
@@ -68,7 +70,8 @@
 			          	  	<span style="color:#b5b3b3;"><i class="fas fa-check-circle" style="color:#0ebff6"></i>&nbsp;Remember me</span>
 			          		<span style="color:#b5b3b3;cursor: pointer;" @click="resetPassword()" >Forgot password?</span>
 			          	</v-card-title>
-			          	<div style="margin-top:100px;">
+			        </v-card>
+					<div style="margin-top:15px;">
 			          		<v-card-title class="right-col-login--action">
 				          		<span class="style-span">Term & Conditions</span>
 				          		<span class="style-span">Privacy Policy</span>
@@ -77,9 +80,7 @@
 				          	<v-card-title class="right-col-login--action right-col-login--note">
 				          		<span class="style-span--note">All Rights Reserved @ Holipic 2019</span>
 				          	</v-card-title>
-			          	</div>
-			          	
-			        </v-card>
+			        </div>
 				</div>
 			</div>
 		</div>
